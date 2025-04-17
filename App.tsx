2977c65b875e71b9d35a595041ca931a67a45566
Ahmed <a.amiratt12@gmail.com>
2025-04-16 19:44:50 -0700

    assistant and analytics page

--- App.tsx
+++ App.tsx
@@ -1,130 +1,440 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react';
 import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar,
+  Switch,
+  Image,
+  ImageBackground,
+  SafeAreaView,
+  Dimensions,
 } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import { useNavigation, NavigationProp } from '@react-navigation/native'; // Import navigation hook
+import { RootStackParamList } from './types'; // Import the type for navigation routes
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// Width and height of the screen
+const { width, height } = Dimensions.get('window');
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+// Image sources
+const images = {
+  homeBackground: require('./assets/HomeBackground.png'),
+  bedIcon: require('./assets/DailyAlarm_BedIcon.png'),
+  alarmIcon: require('./assets/DailyAlarm_AlarmIcon.png'),
+  settingsIcon: require('./assets/DailyAlarm_SettingsIcon.png'),
+  capybaraImage: require('./assets/CapybaraImage.png'),
+  bottomBarHome: require('./assets/BottomBar_Home.png'),
+  bottomBarStats: require('./assets/BottomBar_Stats.png'),
+  bottomBarAssistant: require('./assets/BottomBar_Assistant.png'),
+  bottomBarAlarm: require('./assets/BottomBar_Alarm.png'),
+};
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const App = () => {
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>(); // Initialize navigation with typed routes
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  // State for switches
+  const [bedTimeEnabled, setBedTimeEnabled] = useState(true);
+  const [alarmEnabled, setAlarmEnabled] = useState(true);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
+  // Days of the week for calendar
+  const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  const dates = [21, 22, 23, 24, 25, 26, 27];
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={images.homeBackground}
+        resizeMode="cover"
+        style={styles.backgroundImage}
+      >
+        <View style={styles.innerContainer}>
+          <StatusBar barStyle="light-content" />
+
+          {/* Header section */}
+          <View style={styles.header}>
+            <View style={styles.headerContent}>
+              <Text style={styles.headerName}>Hunter,</Text>
+              <View style={styles.greetingContainer}>
+                <Text style={styles.greeting}>Good Morning</Text>
+                <Text style={styles.sunEmoji}>🌞</Text>
+              </View>
+            </View>
+          </View>
+
+          {/* Notification Card */}
+          <View style={styles.notificationCard}>
+            <TouchableOpacity style={styles.closeButton}>
+              <Text style={styles.closeButtonText}>×</Text>
+            </TouchableOpacity>
+            <Text style={styles.notificationText}>
+              You have slept <Text style={{ fontWeight: 'bold' }}>9h 13m</Text>
+            </Text>
+            <Text style={styles.notificationSubtext}>
+              That is <Text style={{ fontWeight: 'bold' }}>above</Text> your{' '}
+              <Text style={styles.underlinedText}>recommendation</Text>
+            </Text>
+          </View>
+
+          {/* Sleep Calendar Section */}
+          <Text style={styles.calendarTitle}>Your Sleep Calendar</Text>
+          <View style={styles.calendar}>
+            {daysOfWeek.map((day, index) => (
+              <View key={index} style={styles.calendarColumn}>
+                <Text style={styles.dayText}>{day}</Text>
+                <View
+                  style={[
+                    styles.dateCircle,
+                    dates[index] === 24 ? styles.activeDateCircle : null,
+                  ]}
+                >
+                  <Text
+                    style={[
+                      styles.dateText,
+                      dates[index] === 24 ? styles.activeDateText : null,
+                    ]}
+                  >
+                    {dates[index]}
+                  </Text>
+                </View>
+              </View>
+            ))}
+          </View>
+
+          {/* Sleep Tools Section */}
+          <View style={styles.toolsContainer}>
+            {/* Bed Time Card */}
+            <View style={styles.toolCard}>
+              <View style={styles.toolHeader}>
+                <Image
+                  style={styles.toolIconContainer}
+                  source={images.bedIcon}
+                />
+                <TouchableOpacity>
+                  <Image
+                    style={styles.toolSettingsIcon}
+                    source={images.settingsIcon}
+                  />
+                </TouchableOpacity>
+              </View>
+              <Text style={styles.toolTitle}>Bed time</Text>
+              <Text style={styles.toolTime}>7H and 28Min</Text>
+              <Switch
+                value={bedTimeEnabled}
+                onValueChange={setBedTimeEnabled}
+                trackColor={{ false: '#3e3e3e', true: '#50D890' }}
+                thumbColor="#fff"
+                style={styles.switch}
+              />
+            </View>
+
+            {/* Alarm Card */}
+            <View style={styles.toolCard}>
+              <View style={styles.toolHeader}>
+                <Image
+                  style={styles.toolIconContainer}
+                  source={images.alarmIcon}
+                />
+                <TouchableOpacity>
+                  <Image
+                    style={styles.toolSettingsIcon}
+                    source={images.settingsIcon}
+                  />
+                </TouchableOpacity>
+              </View>
+              <Text style={styles.toolTitle}>Alarm</Text>
+              <Text style={styles.toolTime}>16H and 18Min</Text>
+              <Switch
+                value={alarmEnabled}
+                onValueChange={setAlarmEnabled}
+                trackColor={{ false: '#3e3e3e', true: '#50D890' }}
+                thumbColor="#fff"
+                style={styles.switch}
+              />
+            </View>
+          </View>
+
+          {/* Sleep Problem Card */}
+          <View style={styles.problemCard}>
+            <View>
+              <Text style={styles.problemQuestion}>Have a problem</Text>
+              <Text style={styles.problemTitle}>Sleeping?</Text>
+              <TouchableOpacity
+                style={styles.assistantButton}
+                onPress={() => navigation.navigate('Assistant')} // Navigate to Assistant
+              >
+                <Text style={styles.assistantButtonText}>
+                  Go to Assistant
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.sleepImageContainer}>
+              <Image
+                source={images.capybaraImage}
+                style={styles.sleepImage}
+              />
+            </View>
+          </View>
+
+          {/* Bottom Navigation */}
+          <View style={styles.bottomNav}>
+            <TouchableOpacity
+              style={styles.navSelectedItem}
+              onPress={() => navigation.navigate('Home')} // Navigate to App.tsx
+            >
+              <Image source={images.bottomBarHome} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.navItem}
+              onPress={() => navigation.navigate('Analytics')} // Navigate to Analytics.tsx
+            >
+              <Image source={images.bottomBarStats} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.navItem}
+              onPress={() => navigation.navigate('Assistant')} // Navigate to Assistant.tsx
+            >
+              <Image source={images.bottomBarAssistant} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.navItem}
+              onPress={() => navigation.navigate('Alarm')} // Navigate to Alarm.tsx
+            >
+              <Image source={images.bottomBarAlarm} />
+            </TouchableOpacity>
+          </View>
         </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </View>
+      </ImageBackground>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: 'rgba(22, 19, 89, 1)',
+    padding: 0,
   },
-  sectionTitle: {
+  innerContainer: {
+    flex: 1,
+    padding: 20,
+    paddingBottom: height * 0.1, // Add padding to avoid overlap with bottom navigation
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    marginBottom: height * 0.02,
+  },
+  headerContent: {
+    flex: 1,
+  },
+  headerName: {
+    color: '#AAAACC',
+    fontSize: 16,
+  },
+  greetingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  greeting: {
+    color: 'white',
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: 'bold',
   },
-  sectionDescription: {
-    marginTop: 8,
+  sunEmoji: {
+    fontSize: 22,
+    marginLeft: 8,
+  },
+  bellButton: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    backgroundColor: '#807946',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  notificationCard: {
+    backgroundColor: 'rgba(11, 97, 150, 0.7)',
+    borderRadius: 16,
+    padding: height * 0.06,
+    marginBottom: height * 0.02,
+    position: 'relative',
+  },
+  closeButton: {
+    position: 'absolute',
+    top: height * 0.01,
+    right: height * 0.02,
+  },
+  closeButtonText: {
+    color: 'white',
+    fontSize: 24,
+  },
+  notificationText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  notificationSubtext: {
+    color: 'white',
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  underlinedText: {
+    textDecorationLine: 'underline',
+  },
+  calendarTitle: {
+    color: 'white',
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: 'bold',
+    marginBottom: height * 0.015,
   },
-  highlight: {
-    fontWeight: '700',
+  calendar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: height * 0.02,
+  },
+  calendarColumn: {
+    alignItems: 'center',
+  },
+  dayText: {
+    color: '#AAA',
+    marginBottom: height * 0.008,
+  },
+  dateCircle: {
+    width: height * 0.04,
+    height: height * 0.04,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  activeDateCircle: {
+    backgroundColor: 'white',
+  },
+  dateText: {
+    color: '#AAA',
+  },
+  activeDateText: {
+    color: '#1E1F38',
+    fontWeight: 'bold',
+  },
+  toolsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: height * 0.02,
+  },
+  toolCard: {
+    backgroundColor: 'rgba(11, 97, 150, 0.4)',
+    borderRadius: 16,
+    padding: height * 0.02,
+    width: '48%',
+  },
+  toolHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: height * 0.01,
+  },
+  toolIconContainer: {
+    width: '25%',
+    borderRadius: 20,
+    aspectRatio: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  toolSettingsIcon: {
+    borderRadius: 20,
+    opacity: 0.75,
+    top: 10,
+    right: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  toolTitle: {
+    color: 'white',
+    fontSize: 24,
+    width: '75%',
+    fontWeight: 'bold',
+  },
+  toolTime: {
+    color: '#FFF',
+    opacity: 0.4,
+    fontSize: 14,
+    width: '75%',
+    marginBottom: 12,
+  },
+  switch: {
+    opacity: 0.95,
+    alignSelf: 'flex-start',
+    transform: [{ scaleX: 1.5 }, { scaleY: 1.5 }],
+    marginBottom: 10,
+  },
+  problemCard: {
+    backgroundColor: 'rgba(5, 161, 132, 0.4)',
+    borderRadius: 16,
+    padding: height * 0.025,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 24,
+  },
+  problemQuestion: {
+    color: '#FFF',
+    opacity: 0.4,
+    fontSize: 16,
+    marginBottom: -height * 0.005,
+  },
+  problemTitle: {
+    color: 'white',
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: height * 0.02,
+  },
+  assistantButton: {
+    backgroundColor: 'rgba(4, 217, 143, 0.75)',
+    borderRadius: 8,
+    paddingVertical: height * 0.008,
+    opacity: 0.95,
+    paddingHorizontal: width * 0.06,
+    alignSelf: 'flex-start',
+  },
+  assistantButtonText: {
+    fontSize: 16,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  sleepImageContainer: {
+    justifyContent: 'center',
+  },
+  sleepImage: {
+    width: 160,
+    height: 120,
+    resizeMode: 'contain',
+  },
+  bottomNav: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: 'rgba(38, 32, 153, 0.5)',
+    borderRadius: 16,
+    paddingVertical: height * 0.02, // Use relative height for padding
+    paddingHorizontal: width * 0.05, // Use relative width for padding
+    position: 'absolute', // Ensure it stays at the bottom
+    bottom: 0, // Align to the bottom of the screen
+    width: '100%', // Ensure it spans the full width of the screen
+  },
+  navItem: {
+    opacity: 0.5,
+    flex: 1, // Distribute space evenly among navigation items
+    alignItems: 'center', // Center the content horizontally
+  },
+  navSelectedItem: {
+    opacity: 1,
+    flex: 1, // Distribute space evenly among navigation items
+    alignItems: 'center', // Center the content horizontally
   },
 });
 
--- README.md
+++ README.md
@@ -1,26 +1,19 @@
 This is a new [**React Native**](https://reactnative.dev) project, bootstrapped using [`@react-native-community/cli`](https://github.com/react-native-community/cli).
 
+
 # Getting Started
 
 > **Note**: Make sure you have completed the [Set Up Your Environment](https://reactnative.dev/docs/set-up-your-environment) guide before proceeding.
 
-## Step 1: Start Metro
+## Step 1: Run Emulator
 
-First, you will need to run **Metro**, the JavaScript build tool for React Native.
-
-To start the Metro dev server, run the following command from the root of your React Native project:
-
-```sh
-# Using npm
-npm start
-
-# OR using Yarn
-yarn start
-```
+Have your emulator running (Andriod Studio)
+You can run "emulator -list-avds" in terminal to see if your emulator is running. If it comes back empty, 
+something is wrong.
 
 ## Step 2: Build and run your app
 
-With Metro running, open a new terminal window/pane from the root of your React Native project, and use one of the following commands to build and run your Android or iOS app:
+With the emulator running, open terminal and use one of the following commands to build and run Android app
 
 ### Android
 
@@ -32,35 +25,9 @@ npm run android
 yarn android
 ```
 
-### iOS
-
-For iOS, remember to install CocoaPods dependencies (this only needs to be run on first clone or after updating native deps).
-
-The first time you create a new project, run the Ruby bundler to install CocoaPods itself:
-
-```sh
-bundle install
-```
-
-Then, and every time you update your native dependencies, run:
-
-```sh
-bundle exec pod install
-```
-
-For more information, please visit [CocoaPods Getting Started guide](https://guides.cocoapods.org/using/getting-started.html).
-
-```sh
-# Using npm
-npm run ios
-
-# OR using Yarn
-yarn ios
-```
-
-If everything is set up correctly, you should see your new app running in the Android Emulator, iOS Simulator, or your connected device.
-
-This is one way to run your app — you can also build it directly from Android Studio or Xcode.
+Common Issues:
+Make sure in gradle/local.properties has your sdk and ndk path (can find that on Andriod Studio)
+In app/build.gradle make sure your ndk version(under andrioid) is the right one (you can check by going AppData\Local\Android\Sdk\ndk and seeing the file names)
 
 ## Step 3: Modify your app
 
@@ -71,16 +38,6 @@ Open `App.tsx` in your text editor of choice and make some changes. When you sav
 When you want to forcefully reload, for example to reset the state of your app, you can perform a full reload:
 
 - **Android**: Press the <kbd>R</kbd> key twice or select **"Reload"** from the **Dev Menu**, accessed via <kbd>Ctrl</kbd> + <kbd>M</kbd> (Windows/Linux) or <kbd>Cmd ⌘</kbd> + <kbd>M</kbd> (macOS).
-- **iOS**: Press <kbd>R</kbd> in iOS Simulator.
-
-## Congratulations! :tada:
-
-You've successfully run and modified your React Native App. :partying_face:
-
-### Now what?
-
-- If you want to add this new React Native code to an existing application, check out the [Integration guide](https://reactnative.dev/docs/integration-with-existing-apps).
-- If you're curious to learn more about React Native, check out the [docs](https://reactnative.dev/docs/getting-started).
 
 # Troubleshooting
 
